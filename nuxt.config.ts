813d1dbe8255783210a67165ecde38c44b0dffb0
Gabriel Estrela <149545964+Improhalker@users.noreply.github.com>
2025-04-21 21:31:08 -0300

    chore: add @nuxtjs/supabase as a dev dependency in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon', '@nuxt/image'],
+  modules: ['@nuxt/ui', '@nuxt/icon', '@nuxt/image', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false
+  },
   css: ['~/assets/css/main.css']
 })
