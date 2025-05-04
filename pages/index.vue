63b2950858f3f5f642222a88abba4f046af99dec
Gabriel Estrela <149545964+Improhalker@users.noreply.github.com>
2025-05-04 20:19:38 -0300

    feat: enhance transaction data handling by grouping transactions by date

--- pages/index.vue
+++ pages/index.vue
@@ -35,11 +35,27 @@ const { data, pending } = await useAsyncData('transactions', async () => {
         .from('transactions')
         .select()
 
-        if(error) return []
+    if (error) return []
 
-        console.log(data)
-        return data
+    console.log(data)
+    return data
 })
 
 transactions.value = data.value
+
+const transactionsGroupedByDate = computed(() => {
+    let grouped = {}
+
+    for (const transaction of transactions.value) {
+        const date = new Date(transaction.created_at).toISOString().split('T')[0]
+        if (!grouped[date]) {
+            grouped[date] = []
+        }
+        grouped[date].push(transaction)
+    }
+    return grouped
+})
+
+console.log(transactionsGroupedByDate.value)
+
 </script>
