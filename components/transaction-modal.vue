fba9d8ba6d4c21249c31baa77afa7a3c90ed267e
Gabriel Estrela <149545964+Improhalker@users.noreply.github.com>
2025-05-12 20:56:22 -0300

    feat: enhance transaction modal with form fields for type, amount, date, description, and category

--- components/transaction-modal.vue
+++ components/transaction-modal.vue
@@ -2,16 +2,40 @@
   <div>
     <UModal v-model:open="isOpen">
       <template #header>
-        Add Transaction
+        <p>oi</p>
       </template>
       <template #content>
-        Hello!
+        <div class="flex flex-col gap-4 p-4">
+          <UFormField :required="true" label="Transaction Type" name="type" class="mb-4">
+            <USelect placeholder="Select the Transaction Type" :items="types"/>
+          </UFormField>
+
+
+          <UFormField label="Amount" :required="true" name="amount" class="mb-4">
+            <UInput type="number" placeholder="Enter amount" />
+          </UFormField>
+          <UFormField label="Transaction date" :required="true" name="created_at" class="mb-4">
+            <UInput type="date" icon="i-heroicons-calendars-days-20-solid" />
+          </UFormField>
+          <UFormField label="Description" hint="Optional" name="description" class="mb-4">
+            <UInput placeholder="Description"/>
+          </UFormField>
+          <UFormField :required="true" label="Category" name="category" class="mb-4">
+            <USelect placeholder="Category" :items="categories"/>
+          </UFormField>
+
+          <UButton type="submit" color="info" variant="solid" label="Save"/>
+        </div>
       </template>
     </UModal>
   </div>
 </template>
 
+
 <script lang="ts" setup>
+import { categories } from '~/constants';
+import { types } from '~/constants';
+
 const props = defineProps({
   modelValue: {
     type: Boolean,
